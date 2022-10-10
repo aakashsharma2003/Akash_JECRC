73ae8dac4d6e130e4dce67ae0700234617907ed9
aakashsharma2003 <aakashsharma3090@gmail.com>
2022-10-10 01:33:48 -0700

    First commit

---
+++ abc.py
@@ -0,0 +1,15 @@
+##t=[12,15,18,20,8,10,16]
+##
+##for i in t:
+##    if i%2==0 and i>10:
+##        print(i)
+##
+##c=10
+##while (c>-1):
+##    c-=4
+##    print(c)
+##    
+for i in[1,2,'abc','xyz',23,20]:
+    if i=='xyz':
+      break
+    print(i)
